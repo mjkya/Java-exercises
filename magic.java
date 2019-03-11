20f967f9a457a4cff5f245bac360f8643826332e
mjkya <39973760+mjkya@users.noreply.github.com>
2019-03-12 01:01:25 +0900

    Add files via upload

---
+++ magic.java
@@ -0,0 +1,10 @@
+public class test{
+    public int isMultiple(String[] args){
+        int n;
+        int m;
+        int i;
+        if (n == m*i);{
+            return true;
+        }
+    }
+}
